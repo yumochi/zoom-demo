09ced4944961cd5a0190c5a3dfbc40e19dde0347
Yumo Chi <yumo.chi@reconstructinc.com>
2021-11-09 23:09:01 -0600

    added some toy code to allow user to join a hard set meeting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ZoomMtgEmbedded from "@zoomus/websdk/embedded";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const apiKey = "Z0SooeRWTYeW-lMosu6Esw";
+const apiSecret = "zQ6vOkhNn1ipdNFuvsxyH2W0kY2IxO30i7WX";
+const meetingNumber = 9996965760;
+const role = 0;
+
+const client = ZoomMtgEmbedded.createClient();
+
+const crypto = require('crypto') // crypto comes with Node.js
+
+function generateSignature() {
+  // Prevent time sync issue between client signature generation and zoom 
+  const timestamp = new Date().getTime() - 30000
+  const msg = Buffer.from(apiKey + meetingNumber + timestamp + role).toString('base64')
+  const hash = crypto.createHmac('sha256', apiSecret).update(msg).digest('base64')
+  const signature = Buffer.from(`${apiKey}.${meetingNumber}.${timestamp}.${role}.${hash}`).toString('base64')
+
+  return signature
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    window.test = this;
+  }
+
+  generateSignature() {
+    const signature = generateSignature();
+    console.log(signature);
+    return signature;
+  }
+
+  componentDidMount() {
+    let meetingSDKElement = document.getElementById('meetingSDKElement');
+
+    client.init({
+      debug: true,
+      zoomAppRoot: meetingSDKElement,
+      language: 'en-US',
+      customize: {
+        meetingInfo: ['topic', 'host', 'mn', 'pwd', 'telPwd', 'invite', 'participant', 'dc', 'enctype'],
+        toolbar: {
+          buttons: [
+            {
+              text: 'Custom Button',
+              className: 'CustomButton',
+              onClick: () => {
+                console.log('custom button');
+              }
+            }
+          ]
+        }
+      }
+    });
+  }
+
+  join() {
+    const apiKey = "Z0SooeRWTYeW-lMosu6Esw";
+    const meetingNumber = 9996965760;
+    const signature = this.generateSignature();
+    client.join({
+      apiKey: apiKey,
+      signature: signature,
+      meetingNumber: meetingNumber,
+      password: "e46KK0",
+      userName: "Yumo Chi2"
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <div id="meetingSDKElement">
+            {/* <!-- Zoom Meeting SDK Rendered Here --> */}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
